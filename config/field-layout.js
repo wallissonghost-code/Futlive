887fda7fdad066541b21f8be7ba30f01a0b5ea48
ghostzinho <wallissonghost@gmail.com>
2026-08-26 14:11:25 -0300

    chore: separa configuração visual do campo

---
+++ config/field-layout.js
@@ -0,0 +1,11 @@
+window.FutLiveFieldLayout=Object.freeze({
+  mode:'portrait-horizontal-play',
+  artMode:'css-safe',
+  note:'O mapa 3:2 enviado permanece reservado em assets/maps. Ele não é aplicado diretamente no modo retrato porque distorce a geometria do campo.',
+  goalOrientation:'left-right',
+  visualAssets:{
+    map:'./assets/maps/16F9F5B2-A968-4629-A9C0-6ABF47EB5B94.jpeg',
+    leftGoal:'./assets/goals/frame_001.png',
+    rightGoal:'./assets/goals/frame_003.png'
+  }
+});
