573f1a7bf38886a36b69bee10d3fde06f7a87046
ghostzinho <wallissonghost@gmail.com>
2026-08-26 15:21:37 -0300

    feat: inicia beta 0.32 com formacao mais aberta

---
+++ config/lineup-v032.js
@@ -0,0 +1,7 @@
+window.FutLiveLineup=Object.freeze({
+  version:'0.32',
+  teams:{
+    blue:{field:['player1','player2','player3','player4'],goalkeeper:'player5',formation:[[.19,.25],[.27,.74],[.42,.34],[.45,.66],[.055,.50]]},
+    red:{field:['player6','player7','player8','player9'],goalkeeper:'player10',formation:[[.81,.75],[.73,.26],[.58,.66],[.55,.34],[.945,.50]]}
+  }
+});
