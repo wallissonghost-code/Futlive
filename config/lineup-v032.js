da7cbeb1c291cd7903c13bda33eec2a9e44b7b06
ghostzinho <wallissonghost@gmail.com>
2026-08-27 10:59:51 -0300

    feat: add two field players per team

--- config/lineup-v032.js
+++ config/lineup-v032.js
@@ -1,7 +1,7 @@
 window.FutLiveLineup=Object.freeze({
   version:'0.32',
   teams:{
-    blue:{field:['player1','player2','player3','player4'],goalkeeper:'player5',formation:[[.19,.25],[.27,.74],[.42,.34],[.45,.66],[.055,.50]]},
-    red:{field:['player6','player7','player8','player9'],goalkeeper:'player10',formation:[[.81,.75],[.73,.26],[.58,.66],[.55,.34],[.945,.50]]}
+    blue:{field:['player1','player2','player3','player4','player11','player12'],goalkeeper:'player5',formation:[[.19,.25],[.27,.74],[.42,.34],[.45,.66],[.055,.50],[.33,.18],[.33,.82]]},
+    red:{field:['player6','player7','player8','player9','player13','player14'],goalkeeper:'player10',formation:[[.81,.75],[.73,.26],[.58,.66],[.55,.34],[.945,.50],[.67,.82],[.67,.18]]}
   }
 });
